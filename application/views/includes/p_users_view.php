2f957088f65b834ad50c20e6989478e16f58f6d3
elorsh <63967367+elorsh@users.noreply.github.com>
2021-04-27 18:49:48 +0300

    base url

--- application/views/includes/p_users_view.php
+++ application/views/includes/p_users_view.php
@@ -10,8 +10,8 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-     <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/APEStyle.css');?> ">
-     <link rel="stylesheet" href="<?php echo base_url('assets/css/myAppointmentsBusiness.css');?>"/> -->
+    
+     <link rel="stylesheet" href="<?php echo base_url('assets/css/myAppointmentsBusiness.css');?>"/>
      <link rel="stylesheet" href="assets/css/indexStyle.css"/>
 
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
