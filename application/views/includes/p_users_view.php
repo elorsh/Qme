0fa1668fd3d71e352678602ab83e76985fd1b610
elorsh <63967367+elorsh@users.noreply.github.com>
2021-04-27 13:23:20 +0300

    deleted assets folder and moved css, js,img folder

--- application/views/includes/p_users_view.php
+++ application/views/includes/p_users_view.php
@@ -10,7 +10,7 @@
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-     <link rel="stylesheet" href="../assets/css/myAppointmentsBusiness.css"/>
+     <link rel="stylesheet" href="../css/myAppointmentsBusiness.css"/>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
      <title>Private users view</title>
@@ -30,7 +30,7 @@
 
 
     <header> 
-        <img id="logo" src= "../assets/css/img/logo.png"/>
+        <img id="logo" src= "../css/img/logo.png"/>
      </header>
 
      <i class="fas fa-share" onclick="document.location='#'"></i>
