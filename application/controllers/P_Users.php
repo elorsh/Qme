8c54393fc21fb9eab757f363efaec1aad374cd72
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-02 23:24:17 +0300

    fix2

--- application/controllers/P_Users.php
+++ application/controllers/P_Users.php
@@ -41,22 +41,22 @@ class P_Users extends CI_Controller{
 
     public function update(){
 
-        $u_full_name="אבי רון";
         $u_email="avi@qme.com";
+
+        $u_full_name="אבי רון";
         $u_password="123456";
         $u_phone="0505111183";
-        $u_address="הפלמח 8 רמהש";
+        $u_address="הפלמח 8 רמת השרון";
 
 
         $this->P_Users_model->update_p_users([
 
             'u_full_name'=>$u_full_name,
-            'u_email'=>$u_email,
             'u_password'=> $u_password,
             'u_phone'=>$u_phone,
             'u_address'=> $u_address
 
-        ]);
+        ], $u_email);
 
     }
 
