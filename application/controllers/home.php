36918c60e06ec373dbdaf73799b41641a4077989
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-09 17:39:08 +0300

    1

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -12,7 +12,7 @@ class Home extends CI_Controller {
 
     public function index(){
 
-      $data['main_view'] = "home_view";
+      $data['main_view'] = 'home_view';
 
       $this->load->view('main', $data);
     }
