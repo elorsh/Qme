a07b5d8c0098fa904ca68148245b8948a5b02e6e
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-09 17:29:46 +0300

    1

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,6 +1,13 @@
 <?php
 
 class Home extends CI_Controller {
+  public function __construct(){
+    parent::__construct();
+    $this->load->model('intro_model');
+    $this->load->helper('url');
+    $this->load->helper('form');
+    $this->load->library('form_validation');
+    $this->load->library('session');
 
 
     public function index(){
