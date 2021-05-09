d1175894a0b1ca39b6a0b04ca2223ea43979368f
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-09 16:48:23 +0300

    fixing style for p users

--- application/views/includes/p_users_view.php
+++ application/views/includes/p_users_view.php
@@ -11,7 +11,7 @@
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     
-     <link rel="stylesheet" href="<?php echo base_url('assets/css/myAppointmentsBusiness.css');?>"/>
+     <link rel="stylesheet" href="<?php echo base_url('assets/css/B_myAppointmentsStyle.css');?>"/>
 
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
