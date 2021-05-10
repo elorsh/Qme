e081b3bdd870db7a1599a45a8bc3089d68c71068
Lior Aviry <aviryl@gmail.com>
2021-05-10 11:23:50 +0300

    change register css links

--- application/views/includes/B_register_view.php
+++ application/views/includes/B_register_view.php
@@ -10,13 +10,14 @@
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-     <link rel="stylesheet" href="../css/createBussinessAccountStyle.css"/>
+     <link rel="stylesheet" href="<?php echo base_url('assets/css/B_createAccountStyle.css');?>"/>
+
 
      <title>Business Register </title>
  </head>
  <body>
     <header> 
-        <img id="logo" src= "../css/img/logo.png"/>
+        <img id="logo" src= "<?php echo base_url('assets/css/img/logo.png');?>"/>
      </header>
      <i class="fas fa-share" onclick="document.location='businessLogIn.html'"></i>
      <p class="back">חזור</p>
