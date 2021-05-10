e12b08f8873c40001fb1f19a8707035e07cdb5de
Lior Aviry <aviryl@gmail.com>
2021-05-10 15:17:02 +0300

    create search result page

---
+++ application/views/includes/search_result_view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"/>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+     <link rel="stylesheet" href="<?php echo base_url('assets/css/searchResultStyle.css');?>"/>
+
+     <title>Search Result</title>
+ </head>
+ <body>
+  <header> 
+         <img id="logo" src= "<?php echo base_url('assets/css/img/logo.png');?>"/>
+       </header>
+
+     
+ <i class="fas fa-share" onclick="document.location='myProfilePrivate.html'"></i>
+     <p class="back">חזור</p>
+
+     <h4  dir="rtl">תוצאות חיפוש:</h4>
+
+     <h5   dir="rtl" >מספרה ועיצוב שיער</h5> 
+
+     <div class="business-container col-9">
+
+      <h6 dir="rtl" class="business-name">ג'וני עיצוב שיער</h6>
+      <h7 dir="rtl" class="business-details">תיאור בית העסק</h7>
+
+      <p2 dir="rtl" class="business-address">כתובת: <br> 
+      בן יהודה 70 תל אביב- יפו</p2>
+
+      <a href="#" class="link-to calender">זימון תור <i class="far fa-calendar-alt"></i></a>
+      <a href="#" class="link-to Waze">ניווט לבית העסק  <i class="fab fa-waze"></i></a>
+      <a href="#" class="link-to phone">התקשר לבית העסק <i class="fas fa-phone"></i></a>
+
+
+
+
+     </div>
+ </body>
+ </html>
