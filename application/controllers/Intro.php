5168bf231b501911d349bcc8869d12dc1b233861
Lior Aviry <aviryl@gmail.com>
2021-05-11 15:50:53 +0300

    b_editAppointment links

--- application/controllers/Intro.php
+++ application/controllers/Intro.php
@@ -27,7 +27,22 @@ class Intro extends CI_Controller{
     public function go_to_b_register(){
         $this->load->view('includes/B_register_view');
         }
-
+    public function go_to_home_page(){
+            $this->load->view('includes/homePage_view');
+        }
+    public function go_to_b_appointments(){
+            $this->load->view('includes/B_myAppointments_view');
+        }
+     public function go_to_b_myProfile(){
+            $this->load->view('includes/B_myProfile_view');
+        }
+    public function go_to_b_create_appointment(){
+            $this->load->view('includes/B_createAppointment_view');
+        }
+        public function go_to_b_cancel_appointment(){
+            $this->load->view('includes/B_cancelAppointment_view');
+        }
+        
     }
 
     
