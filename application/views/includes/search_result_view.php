01df81371ece82fd0ed1cbade260cbd88ef890a2
Lior Aviry <aviryl@gmail.com>
2021-05-11 15:07:16 +0300

    editt search result page

--- application/views/includes/search_result_view.php
+++ application/views/includes/search_result_view.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
  <html lang="en">
  <head>
@@ -10,13 +11,12 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
      <link rel="stylesheet" href="<?php echo base_url('assets/css/searchResultStyle.css');?>"/>
-
      <title>Search Result</title>
  </head>
  <body>
   <header> 
-         <img id="logo" src= "<?php echo base_url('assets/css/img/logo.png');?>"/>
-       </header>
+        <img id="logo" src= "<?php echo base_url('assets/css/img/logo.png');?>"/>
+    </header>
 
      
  <i class="fas fa-share" onclick="document.location='myProfilePrivate.html'"></i>
@@ -29,18 +29,28 @@
      <div class="business-container col-9">
 
       <h6 dir="rtl" class="business-name">ג'וני עיצוב שיער</h6>
-      <h7 dir="rtl" class="business-details">תיאור בית העסק</h7>
-
-      <p2 dir="rtl" class="business-address">כתובת: <br> 
-      בן יהודה 70 תל אביב- יפו</p2>
-
-      <a href="#" class="link-to calender">זימון תור <i class="far fa-calendar-alt"></i></a>
-      <a href="#" class="link-to Waze">ניווט לבית העסק  <i class="fab fa-waze"></i></a>
-      <a href="#" class="link-to phone">התקשר לבית העסק <i class="fas fa-phone"></i></a>
+        <img src="img1.jpg" contenteditable="img1" width="200px"
+        height="130px" class="rounded float-right"  alt="image1">
 
 
+      <h7 dir="rtl" class="business-details col-8"> מכון בוטיק לעיצוב שיער </h7>
 
 
-     </div>
+    <div id="clear col-12"></div>
+
+<p2 dir="rtl" class="business-address col-8">כתובת: <br> 
+      בן יהודה 70 תל אביב- יפו</p2>  
+
+      <div id="clear col-12"></div>
+
+      <a href="#" class="link-to calender col-2">זימון תור <i class="far fa-calendar-alt"></i></a>
+      <a href="#" class="link-to Waze col-2"> <i class="fab fa-waze"></i> ניווט לבית העסק  </a>
+      <a href="#" class="link-to phone col-2"><i class="fas fa-phone"></i> התקשר לבית העסק </a>
+     
+
+    </div>
+
+
+
  </body>
  </html>
