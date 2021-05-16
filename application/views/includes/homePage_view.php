accffa41feff914e4613e6152a01ee41cdf77661
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-16 16:08:10 +0300

    3

--- application/views/includes/homePage_view.php
+++ application/views/includes/homePage_view.php
@@ -59,7 +59,7 @@
 
                 echo "<tr>";
                 // echo '<td scope="col">'.$object->b_business_name.'</td>';
-                echo '<td scope="col">'.$object->b_business_name.'<br>'.'-'.$object->b_profession.'<br>'.'בכתובת- '.$object->b_address.'</td>';
+                echo '<td scope="col"><h4>'.$object->b_business_name.'</h4><br>'.$object->b_profession.'<br>'.'בכתובת- '.$object->b_address.'</td>';
                 echo '<td scope="col">'.$object->b_description.'</td>';
                 echo '<td scope="col">'.$object->b_address.'</td>';
                 echo '<td><i class="far fa-calendar-alt"></i></td>';
