1bc0c484437602315a3a0b7f03e4a2653c87344d
Lior Aviry <aviryl@gmail.com>
2021-05-16 16:24:50 +0300

    home page h6 change

--- application/views/includes/homePage_view.php
+++ application/views/includes/homePage_view.php
@@ -56,7 +56,7 @@
               foreach ($result as $object){
                 echo "<tr>";
                 // echo '<td scope="col">'.$object->b_business_name.'</td>';
-                echo '<td scope="col"><h4>'.$object->b_business_name.'</h4><br>'.$object->b_profession.'<br>'.$object->b_address.'</td>';
+                echo '<td scope="col"><h5>'.$object->b_business_name.'</h5><br><h6>'.$object->b_profession.'</h6><br><h7>'.$object->b_address.'</h7></td>';
                 echo '<td scope="col">'.$object->b_description.'</td>';
                 echo '<td><i class="far fa-calendar-alt"></i></td>';
                 echo '<td><i class="fab fa-waze"></i></td>';
