d62cf717ec5d1df1faa4b5f57099a62c06851314
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-16 15:51:45 +0300

    1

--- application/views/includes/homePage_view.php
+++ application/views/includes/homePage_view.php
@@ -126,17 +126,6 @@
           table = document.getElementById("myTable");
           tr = table.getElementsByTagName("tr");
           for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-              txtValue = td.textContent || td.innerText;
-              if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-              } else {
-                tr[i].style.display = "none";
-              }   
-          }
-         }
-         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td")[1];
             if (td) {
               txtValue = td.textContent || td.innerText;
