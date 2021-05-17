bb9aaf4f50df3978aca3f74d45616bb2f3fcbdfe
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-17 21:28:22 +0300

    2

--- application/views/includes/homePage_view.php
+++ application/views/includes/homePage_view.php
@@ -55,8 +55,7 @@
               <?php
               foreach ($result as $object){
                 echo "<tr>";
-                // echo '<td scope="col">'.$object->b_business_name.'</td>';
-                echo '<td scope="col"><h5>'.$object->b_business_name.'</h5><br><h6>'.$object->b_profession.'</h6><br><h7>'.$object->b_address.'</h7><br><h7>'.$object->b_description.'</h7></td>';
+                echo '<td scope="col"><h5>'.$object->b_business_name.'</h5><h6>'.$object->b_profession.'</h6><br><h7>'.$object->b_description.'</h7><br><h7>'.$object->b_address.'</h7></td>';
                 // echo '<td scope="col">'.$object->b_description.'</td>';
                 echo '<td><i class="far fa-calendar-alt"></i></td>';
                 echo '<td><i class="fab fa-waze"></i></td>';
