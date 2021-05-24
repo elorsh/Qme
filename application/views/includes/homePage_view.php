b1aac3ad11febedb76a9d64fc4ce4aaab3d5b1e2
Lior Aviry <aviryl@gmail.com>
2021-05-24 17:31:44 +0300

    fix 2

--- application/views/includes/homePage_view.php
+++ application/views/includes/homePage_view.php
@@ -72,7 +72,7 @@
                 echo '<td scope="col"><h5>'.$object->b_business_name.'</h5><h6>'.$object->b_profession.'</h6><h7>'.$object->b_description.'</h7><br><h7>כתובת: '.$object->b_address.'</h7></td>';
                 // echo '<td scope="col">'.$object->b_description.'</td>';
                 echo '<td><i class="far fa-calendar-alt"  id="newAppointment" ></i></td>';
-                echo '<td><a href="https://api.whatsapp.com/send?phone=+972<?php echo $object->b_phone1?>&text=היי20%,20%כיצד20%נוכל%לעזור%?20%" target="_blank">
+                echo '<td><a href="https://api.whatsapp.com/send?phone=+972<?php echo $object->b_phone1?>&text=היי, כיצד נוכל לעזור?" target="_blank">
                 <i class="fab fa-whatsapp"></i></a></td>';
                 echo '<td><i class="fab fa-waze"></i></td>';
                 echo '<td><i class="fas fa-phone"></i></td>';
