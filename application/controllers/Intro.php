dc354b5be6d7114da1fd4d5148e7c1a0a363652f
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-24 18:01:00 +0300

    mooving not important controllers to folder

--- application/controllers/Intro.php
+++ application/controllers/Intro.php
@@ -16,56 +16,56 @@ class Intro extends CI_Controller{
 
         // b user
 
-    public function go_to_b_login(){
-        $this->load->view('includes/B_LogIn_view');
-        }
-    public function go_to_b_register(){
-        $this->load->view('includes/B_register_view');
-        }
+//     public function go_to_b_login(){
+//         $this->load->view('includes/B_LogIn_view');
+//         }
+//     public function go_to_b_register(){
+//         $this->load->view('includes/B_register_view');
+//         }
   
-     public function go_to_b_myProfile(){
-            $this->load->view('includes/B_myProfile_view');
-        }
-    public function go_to_b_create_appointment(){
-            $this->load->view('includes/B_createAppointment_view');
-        }
-    public function go_to_b_cancel_appointment(){
-            $this->load->view('includes/B_cancelAppointment_view');
-        }
-    public function go_to_b_change_password(){
-            $this->load->view('includes/B_changePassword_view');
-        }
-    public function go_to_b_myDetails(){
-            $this->load->view('includes/B_editAccount_view');
-        }
-    public function go_to_b_myCustomers(){
-            $this->load->view('includes/B_myCustomers_view');
-        }
-    public function go_to_private_account(){
-            $this->load->view('includes/P_LogIn_view');
-        }
+//      public function go_to_b_myProfile(){
+//             $this->load->view('includes/B_myProfile_view');
+//         }
+//     public function go_to_b_create_appointment(){
+//             $this->load->view('includes/B_createAppointment_view');
+//         }
+//     public function go_to_b_cancel_appointment(){
+//             $this->load->view('includes/B_cancelAppointment_view');
+//         }
+//     public function go_to_b_change_password(){
+//             $this->load->view('includes/B_changePassword_view');
+//         }
+//     public function go_to_b_myDetails(){
+//             $this->load->view('includes/B_editAccount_view');
+//         }
+//     public function go_to_b_myCustomers(){
+//             $this->load->view('includes/B_myCustomers_view');
+//         }
+//     public function go_to_private_account(){
+//             $this->load->view('includes/P_LogIn_view');
+//         }
         
-// private
+// // private
 
-public function go_to_p_login(){
-    $this->load->view('includes/P_LogIn_view');
-    }
-public function go_to_p_register(){
-    $this->load->view('includes/P_register_view');
-    }
-    
-    public function go_to_P_myProfile(){
-        $this->load->view('includes/P_myProfile_view');
-    }
-    public function go_to_P_create_appointment(){
-        $this->load->view('includes/P_createAppointment_view');
-    }
-    public function go_to_P_cancel_appointment(){
-        $this->load->view('includes/P_cancelAppointment_view');
-    }
-    public function go_to_p_change_password(){
-        $this->load->view('includes/P_changePassword_view');
-    }
+// public function go_to_p_login(){
+//     $this->load->view('includes/P_LogIn_view');
+//     }
+// public function go_to_p_register(){
+//     $this->load->view('includes/P_register_view');
+//     }
+
+//     public function go_to_P_myProfile(){
+//         $this->load->view('includes/P_myProfile_view');
+//     }
+//     public function go_to_P_create_appointment(){
+//         $this->load->view('includes/P_createAppointment_view');
+//     }
+//     public function go_to_P_cancel_appointment(){
+//         $this->load->view('includes/P_cancelAppointment_view');
+//     }
+//     public function go_to_p_change_password(){
+//         $this->load->view('includes/P_changePassword_view');
+//     }
 
 
     
