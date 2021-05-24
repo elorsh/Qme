4dd8f04e9a293697f5fac044e4b6c6b5b1ae2c27
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-24 14:18:06 +0300

    fix

--- application/controllers/P_Users.php
+++ application/controllers/P_Users.php
@@ -242,14 +242,14 @@ public function insert_new_p_user($data){
 //  -----------------B-go_to----------------------
 
 
-// פונקציה שאמורה למשוך את שם המשתמש של הבית עסק מהדב
-// public function go_to_B_myProfile_view(){
-//     $b_user=$this->session->all_userdata(); // לשים בכל פונקציה בקנטרולר כדי להעביר מידע על הסשן
-//     $data['b_user']=$b_user;// כנל
-//     $user_data=$this->b_get_user_data($b_user);
-//     $data['b_business_name']=$user_data['b_business_name']; // לנסות להבין למה הוא לא מצליח למשוך את השם בית עסק
-//     $this->load->view('includes/B_myProfile_view',$data);
-// }
+
+public function go_to_B_myProfile_view(){
+    $b_user=$this->session->all_userdata(); // לשים בכל פונקציה בקנטרולר כדי להעביר מידע על הסשן
+    $data['b_user']=$b_user;// כנל
+    // $user_data=$this->b_get_user_data($b_user); // פונקציה שאמורה למשוך את שם המשתמש של הבית עסק מהדב
+    $data['b_business_name']=$user_data['b_business_name']; // לנסות להבין למה הוא לא מצליח למשוך את השם בית עסק
+    $this->load->view('includes/B_myProfile_view',$data);
+}
 
 
 
