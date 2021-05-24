8f53b89b35f111cb788423fdb0d04a6a3a6cae68
Lior Aviry <aviryl@gmail.com>
2021-05-24 13:37:43 +0300

    email message fix

--- application/views/includes/B_register_view.php
+++ application/views/includes/B_register_view.php
@@ -33,7 +33,7 @@
 
     <?php 
           if (isset($error)) {
-            echo '<center><br> <p4 class="wrongEmail" >'.$error.'</p4></center>';
+            echo '<center><br> <p4 class="wrongEmail">'.$error.'</p4></center>';
           }
       ?>  
    
