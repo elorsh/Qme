ddb638a5bd33c0c4dec63c68079b934f22211480
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-24 20:09:33 +0300

    1

--- application/controllers/P_Users.php
+++ application/controllers/P_Users.php
@@ -96,7 +96,7 @@ public function insert_new_p_user($data){
     }
 
     // עדכון משתמש קיים
-    public function P_update_user($data){
+    public function P_update_user(){
         $data = array(
             'u_full_name' => $this->input->post('u_full_name'),
             'u_phone' => $this->input->post('u_phone'),
