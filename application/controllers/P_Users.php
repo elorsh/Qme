58c8cdd5d20857db876b1685ce3763efa4f140aa
elorsh <63967367+elorsh@users.noreply.github.com>
2021-05-30 12:51:57 +0300

    4

--- application/controllers/P_Users.php
+++ application/controllers/P_Users.php
@@ -99,7 +99,7 @@ public function insert_new_p_user($data){
     public function P_update_user(){
         $p_user=$this->session->all_userdata(); // לשים בכל פונקציה בקנטרולר כדי להעביר מידע על הסשן
         $data = array(
-            'u_email' => $p_user.u_email,
+            'u_email' => $p_user('u_email'),
             'u_full_name' => $this->input->post('u_full_name'),
             'u_phone' => $this->input->post('u_phone'),
             'u_address' => $this->input->post('u_address')
