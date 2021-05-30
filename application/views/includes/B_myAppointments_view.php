ca6fca61c4595efd34c0501d19b38f5a586f1563
Lior Aviry <aviryl@gmail.com>
2021-05-30 16:59:16 +0300

    fix

--- application/views/includes/B_myAppointments_view.php
+++ application/views/includes/B_myAppointments_view.php
@@ -135,9 +135,9 @@
 
     <form  dir="rtl" onsubmit="return validationForm()" >
 
-<div class="form-group ">
-<label for="date">תאריך : </label>
-<select type="date" class="form-select" name="a_date" id="date" required>
+    <div class="form-group ">
+    <label for="date">תאריך : </label>
+    <select type="date" class="form-select" name="a_date" id="date" required>
     <option selected disabled value="">תאריך</option>
     
     <?php
@@ -147,9 +147,9 @@
 
     ?>
 
-    </select>
-</div>
-</form>
+      </select>
+    </div>
+  </form>
 
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
