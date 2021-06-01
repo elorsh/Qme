1692ba0ecfb93efde1f74f94c0379772b6afbf60
elorsh <63967367+elorsh@users.noreply.github.com>
2021-06-01 21:33:48 +0300

    WORKING!!!!!

--- application/views/includes/B_myAppointments_view.php
+++ application/views/includes/B_myAppointments_view.php
@@ -126,6 +126,7 @@
 
     <button class="btn"  id="createAppointment" type="button" > <i class="far fa-calendar-alt"></i>הזנה למערכת תור חדש </button>
 
+    <h4 class="headline">ביטול תור</h4>
     <form class="form1" dir="rtl" method="post"  action="<?php echo site_url('P_Users/go_to_b_cancelAppointment');?>"  onsubmit="return validationForm()" >
 
     <div class="form-group">
