d23a62a9c3c1260eac13a7a053fabdbeea7ee501
elorsh <63967367+elorsh@users.noreply.github.com>
2021-06-03 18:25:57 +0300

    2

--- application/views/includes/P_editAccount_view.php
+++ application/views/includes/P_editAccount_view.php
@@ -74,16 +74,15 @@
 
     <div class="createAccount">
    
-      <!-- <form  dir="rtl"  class="row g-3 needs-validation"  action="<?php echo site_url('P_Users/P_update_user')?>" novalidate> -->
       <form  dir="rtl" onsubmit="return  validationForm()" method="post"  action="<?php echo site_url('P_Users/P_update_user');?>" >
 
-
+<!-- 
       <input type="text" class="form-control" name="u_full_name" id="validationCustom01" placeholder="שם פרטי ומשפחה" >
       <input type="text" class="form-control" name="u_phone" id="validationCustom01" placeholder="שם פרטי ומשפחה" >
       <input type="text" class="form-control" name="u_address" id="validationCustom01" placeholder="שם פרטי ומשפחה" >
-      <button class="btn"  id="submit" type="submit" >בחר תור זה</button>
+      <button class="btn"  id="submit" type="submit" >בחר תור זה</button> -->
 
-        <!-- <div class="col-md-4" >
+        <div class="col-md-4" >
           <label for="validationCustom01" class="form-label" >שם מלא :</label>
           <input type="text" class="form-control" name="u_full_name" id="validationCustom01" value= "<?php foreach ($result as $res){if ($res->u_full_name!=null){echo $res->u_full_name;}} ?>" placeholder="שם פרטי ומשפחה" >
           </div>
@@ -96,11 +95,11 @@
         <div class="col-md-3">
             <label for="validationCustom02" class="form-label">כתובת :</label>
             <input type="text" class="form-control" name="u_address" id="validationCustom05" value= "<?php foreach ($result as $res){if ($res->u_address!=null){echo $res->u_address;}} ?>" placeholder="עיר ,רחוב ,מספר בית" >
-          </div> -->
+          </div>
           
       
 
-        <!-- <button class="btn btn update "  id="submit" type="submit" >  עדכן את הפרטים שלי</button> -->
+        <button class="btn btn update "  id="submit" type="submit" >  עדכן את הפרטים שלי</button>
 
       </form>
       <div class="col-12">
